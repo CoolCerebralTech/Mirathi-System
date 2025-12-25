876bad73438560464f2ff01bbf839f2ea258aa00
Howkins Oyugi <juniorhowkins@gmail.com>
2025-12-25 08:58:42 +0300

    development of the system

--- shambasure-backend/apps/family-service/src/application/guardianship/queries/read-models/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './annual-report-status.read-model';
-export * from './compliance-status.read-model';
-export * from './bond-expiry.read-model';
-export * from './guardianship-details.read-model';
-export * from './customary-guardianship.read-model';
-export * from './guardian-assignment.read-model';
-export * from './guardianship-summary.read-model';
